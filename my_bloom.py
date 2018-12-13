b6c5bd1144d2b07dda7e560119f72137c008183b
jlgunnin <jason.l.gunnink@uscis.dhs.gov>
2018-12-13 13:37:33 -0500

    initial commit

--- my_bloom.py
+++ my_bloom.py
@@ -4,7 +4,23 @@ import mmh3
 from bitarray import bitarray
 from sklearn.model_selection import train_test_split
 from math import log, log2
-from utils import read_words, time_it
+
+def read_words(words_file):
+    """ Create a list object of the lines read from a file """
+    return [word for line in open(words_file, 'r') for word in line.split()]
+
+def time_it(func):
+    """ Decorator function to calculate elapsed time """
+    import time
+    def wrapper(*args, **kwargs):
+        print("Timing:", str(func.__doc__))
+        print("---------------")
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        elapsed_time = time.perf_counter() - start_time
+        print("---------------")
+        print("Elaspsed time:", str(elapsed_time), "seconds")
+    return wrapper
 
 class myBloomFilter:
     """ Bloom filter object """
